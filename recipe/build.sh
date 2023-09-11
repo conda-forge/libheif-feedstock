c3153cabd686ed7e653ea9a33d2bce083c700f1b
Mark Harfouche <mark.harfouche@gmail.com>
2023-09-10 21:46:10 -0400

    disable previously disabled encoders

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,8 @@ cmake ${CMAKE_ARGS} -G "Ninja" \
   -DCMAKE_SYSTEM_PREFIX_PATH="$PREFIX" \
   -DCMAKE_BUILD_TYPE="Release" \
   -DWITH_EXAMPLES=OFF \
+  -DWITH_SvtEnc=OFF \
+  -DWITH_RAV1E=OFF \
   ..
 
 ninja
